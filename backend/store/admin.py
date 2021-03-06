1fa4e5f7f122118703e67ba9b4216ceb2692fe4f
Prakhar Rathi <38958532+prakharrathi25@users.noreply.github.com>
2021-03-06 14:21:35 +0530

    Category Model Added and Migrated to the platform
    Category Model added as a foreign key to the Product Model. Dummy entry added in the product model.

--- backend/store/admin.py
+++ backend/store/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Product)
+admin.site.register(Category)
+
+
+
+
+# Automatic model registration
+# from django.apps import apps
+#
+# models = apps.get_models()
+# print(models)
+# for model in models:
+#     admin.site.register(model)
--- backend/store/models.py
+++ backend/store/models.py
@@ -3,13 +3,23 @@ from django.db import models
 # Create your models here.
 
 
-# Product model to store the details of all the projects 
+# Category Model to store the details of categories to which products belong
+class Category(models.Model):
+
+	# Define plural name using meta class
+	class Meta:
+		verbose_name_plural = "categories"
+
+	# Define the fields of the category model
+	name = models.CharField(max_length=50)
+
+# Product model to store the details of all the products
 class Product(models.Model):
 
-	# Define the fields of the product model 
-	name = models.CharField(max_length=50)
+	# Define the fields of the product model
+	name = models.CharField(max_length=100)
 	price = models.IntegerField(default=0)
 	quantity = models.IntegerField(default=0)
 	description = models.CharField(max_length=200, default='')
-	image = models.ImageField(upload_to='products/images/')
-	
+	image = models.ImageField(upload_to='uploads/images/products')
+	category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)    # Foriegn key with Category Model
