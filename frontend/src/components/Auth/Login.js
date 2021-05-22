bdad9f4cb24da9bd5aa2a564c8fe88a5fab14c4f
Kirtik Singh <kirtiksingh8@gmail.com>
2021-05-22 06:11:17 +0530

    Login, Registered Connected to API

--- frontend/src/components/Auth/Login.js
+++ frontend/src/components/Auth/Login.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
-import Illustration from '../Auth/login_illus.svg';
+import Illustration from "../Auth/login_illus.svg";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
 
 toast.configure();
 
@@ -23,27 +24,138 @@ const formToggleHandler = () => {
     .classList.remove("active");
 };
 
-const loginSubmit = (e) => {
-  e.preventDefault();
-  console.log("loginSubmit");
-  toast.success("Padhaaro Sa");
-};
-
-const signupSubmit = (e) => {
-  e.preventDefault();
-  console.log("signupSubmit");
-  toast.error("You Can't Sit With Us");
-};
-
 const Login = () => {
+  const [signInFormValues, setSignInFormValues] = useState({
+    email: "",
+    password: "",
+  });
+  const [signUpFormValues, setSignUpFormValues] = useState({
+    name: "",
+    email: "",
+    mobile: "",
+    password: "",
+  });
+
+  // SignIn Handlers
+  const handleSignInEmail = (event) => {
+    event.persist();
+    setSignInFormValues((signInFormValues) => ({
+      ...signInFormValues,
+      email: event.target.value,
+    }));
+  };
+  const handleSignInPassword = (event) => {
+    event.persist();
+    setSignInFormValues((signInFormValues) => ({
+      ...signInFormValues,
+      password: event.target.value,
+    }));
+  };
+
+  // SignUp Handlers
+  const handleSignUpName = (event) => {
+    event.persist();
+    setSignUpFormValues((signUpFormValues) => ({
+      ...signUpFormValues,
+      name: event.target.value,
+    }));
+  };
+  const handleSignUpEmail = (event) => {
+    event.persist();
+    setSignUpFormValues((signUpFormValues) => ({
+      ...signUpFormValues,
+      email: event.target.value,
+    }));
+  };
+  const handleSignUpMobile = (event) => {
+    event.persist();
+    setSignUpFormValues((signUpFormValues) => ({
+      ...signUpFormValues,
+      mobile: event.target.value,
+    }));
+  };
+  const handleSignUpPassword = (event) => {
+    event.persist();
+    setSignUpFormValues((signUpFormValues) => ({
+      ...signUpFormValues,
+      password: event.target.value,
+    }));
+  };
+
+  const loginSubmit = (e) => {
+    e.preventDefault();
+    // console.log("loginSubmit");
+    console.log(signInFormValues);
+
+    var data = JSON.stringify({
+      email: signInFormValues.email,
+      password: signInFormValues.password,
+    });
+
+    var config = {
+      method: "post",
+      url: "http://127.0.0.1:8000/login",
+      headers: {
+        "Content-Type": "application/json",
+        // Cookie:
+        //   "csrftoken=No8wD0cOms43Hh37HiwdjatM4lpChEwbgfLcTTi4gmB1FIQxKrjiujILll3tDA8i",
+      },
+      data: data,
+    };
+
+    axios(config)
+      .then(function (response) {
+        console.log(response.data);
+        toast.success("Padhaaro Sa");
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
+  const signupSubmit = (e) => {
+    e.preventDefault();
+    // console.log("signupSubmit");
+    // console.log(signUpFormValues);
+
+    var axios = require("axios");
+    var data = JSON.stringify({
+      first_name: signUpFormValues.name,
+      last_name: "Singh",
+      email: signUpFormValues.email,
+      phone: signUpFormValues.mobile,
+      password: signUpFormValues.password,
+      password2: signUpFormValues.password,
+    });
+
+    var config = {
+      method: "post",
+      url: "http://127.0.0.1:8000/register",
+      headers: {
+        "Content-Type": "application/json",
+        // Cookie:
+        //   "csrftoken=No8wD0cOms43Hh37HiwdjatM4lpChEwbgfLcTTi4gmB1FIQxKrjiujILll3tDA8i",
+      },
+      data: data,
+    };
+
+    axios(config)
+      .then(function (response) {
+        console.log(response.data);
+        toast.error("You Can't Sit With Us");
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
   return (
     <div className="login-container">
-      
       <div className="form">
-      <div className="login-illus">
-        <img src={Illustration} alt="Login/Sign Up" className="login-svg"/>
-      </div>
-        
+        <div className="login-illus">
+          <img src={Illustration} alt="Login/Sign Up" className="login-svg" />
+        </div>
+
         <div className="form-toggle" onClick={() => formToggleHandler()}></div>
         <div className="form-panel one" style={{ cursor: "pointer" }}>
           <div className="form-header">
@@ -58,6 +170,8 @@ const Login = () => {
                   type="text"
                   name="username"
                   required="required"
+                  onChange={handleSignInEmail}
+                  value={signInFormValues.email}
                 />
               </div>
               <div className="form-group">
@@ -67,6 +181,8 @@ const Login = () => {
                   type="password"
                   name="password"
                   required="required"
+                  onChange={handleSignInPassword}
+                  value={signInFormValues.password}
                 />
               </div>
               <div className="form-group">
@@ -88,15 +204,17 @@ const Login = () => {
           </div>
           <div className="form-content">
             <form onSubmit={signupSubmit}>
-              {/* <div className="form-group">
-                <label htmlFor="username">Username</label>
+              <div className="form-group">
+                <label htmlFor="name">Name</label>
                 <input
-                  id="username"
+                  id="name"
                   type="text"
-                  name="username"
+                  name="name"
                   required="required"
+                  onChange={handleSignUpName}
+                  value={signUpFormValues.name}
                 />
-              </div> */}
+              </div>
               <div className="form-group">
                 <label htmlFor="email">Email Address</label>
                 <input
@@ -104,6 +222,19 @@ const Login = () => {
                   type="email"
                   name="email"
                   required="required"
+                  onChange={handleSignUpEmail}
+                  value={signUpFormValues.email}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="email">Phone Number</label>
+                <input
+                  id="mobile"
+                  type="text"
+                  name="mobile"
+                  required="required"
+                  onChange={handleSignUpMobile}
+                  value={signUpFormValues.mobile}
                 />
               </div>
               <div className="form-group">
@@ -113,17 +244,21 @@ const Login = () => {
                   type="password"
                   name="password"
                   required="required"
+                  onChange={handleSignUpPassword}
+                  value={signUpFormValues.password}
                 />
               </div>
-              <div className="form-group">
+              {/* <div className="form-group">
                 <label htmlFor="cpassword">Confirm Password</label>
                 <input
                   id="cpassword"
                   type="password"
                   name="cpassword"
                   required="required"
+                  onChange={handleEmail}
+                  value={formValues.password}
                 />
-              </div>
+              </div> */}
               <div className="form-group">
                 <button type="submit">Register</button>
               </div>
